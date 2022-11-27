52a98dce7f6676cb4ae54ef140d3a9b4052b76a6
Jeff Gbanziali <jeff.gbanziali@efrei.net>
2022-11-27 23:03:59 +0100

    second

--- src/Components/Home/Banner.js
+++ src/Components/Home/Banner.js
@@ -1,13 +1,14 @@
 import React from 'react'
-import {Autoplay} from 'swiper'
+import { Autoplay } from 'swiper'
 import { Swiper, SwiperSlide } from 'swiper/react';
-import {Movies} from '../../Data/MoviesData'
+import { Movies } from '../../Data/MoviesData'
+import FlexMoviesItems from './FlexMoviesItems';
 
 function Banner() {
     return (
         <div className='relative w-full'>
             <Swiper
-            direction='vertical'
+                direction='vertical'
                 spaceBetween={0}
                 slidesPerView={1}
                 loop={true}
@@ -18,18 +19,26 @@ function Banner() {
                     disableOnInteraction: false
                 }}
                 className='w-full xl:h-96 bg-dry lg:h-64 h-48'
-                >
+            >
 
-                    {
-                        Movies.slice(0.6).map((movie, index) => (
-                            <SwiperSlide key={index} className="relative rounded overflow-hidden">
-                                <img 
-                                src={`/images/movies/${movie.image}`} 
-                                alt={movie.title} 
-                                className='w-full h-full object-cover'/>
-                            </SwiperSlide>
-                        ))
-                    }
+                {
+                    Movies.slice(0.6).map((movie, index) => (
+                        <SwiperSlide key={index} className="relative rounded overflow-hidden">
+                            <img
+                                src={`/images/movies/${movie.image}`}
+                                alt={movie.name}
+                                className='w-full h-full object-cover' />
+                            <div className="absolute linear-bg xl:pl-52 sm:pl-32 pl-8 top-0 flex flex-col justify-center right-0 left-0 lg:gap-8 md:gap-5 gap-4">
+                                <h1 className="text-white text-xl lg:text-3xl xl:text-4xl sm:text-2xl font-sans font-bold truncate capitalize">
+                                    {movie.name}
+                                </h1>
+                                <div className="flex gap-5 items-center text-dryGray">
+                                    <FlexMoviesItems movie={movie}/>
+                                </div>
+                            </div>
+                        </SwiperSlide>
+                    ))
+                }
 
 
 
---
+++ src/Components/Home/FlexMoviesItems.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function FlexMoviesItems(movie) {
+  return (
+    <>
+    <div className="flex items-center gap-2">
+        <span className="text-sm font-medium">{movie.category}</span>
+    </div>
+    </>
+  )
+}
+
+export default FlexMoviesItems
